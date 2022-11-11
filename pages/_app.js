9a2c7a717299e86afd85bdaeacab8e8c28482f5c
JM <59332514+mjosh51@users.noreply.github.com>
2022-11-10 19:25:58 -0600

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import '../styles/globals.css';
+// import type { AppProps } from 'next/app';
+import { ContextProvider } from '../context/context';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ContextProvider
+      value={{
+        user: null,
+        isFetching: false,
+        error: false,
+      }}>
+      <Component {...pageProps} />
+    </ContextProvider>
+  );
+}
+
+export default MyApp;

1dc38f53e3362a4455e9273e371f700c6dceceea
JM <59332514+mjosh51@users.noreply.github.com>
2022-11-10 19:58:38 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "https://bg-api.onrender.com/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },

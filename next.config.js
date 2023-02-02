b2857e83c9ab133ddc5f26e7a75116585f0f3b84
JM <59332514+mjosh51@users.noreply.github.com>
2023-02-02 12:01:55 -0500

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,30 +3,41 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: { ignoreDuringBuilds: true },
+  NEXT_PUBLIC_DISABLE_ERROR_OVERLAY: true,
   env: {
-    API_URI: 'https://bg-api.onrender.com/api',
+    API_URL: process.env.API_URL,
+    POSTS_URL: process.env.POSTS_URL,
+    TIPS_URL: process.env.TIPS_URL,
+    CATEGORIES_URL: process.env.CATEGORIES_URL,
+    USER_DASHBOARD_URL: process.env.USER_DASHBOARD_URL,
+    UPLOAD_IMAGE_URL: process.env.UPLOAD_IMAGE_URL,
+    UPDATE_PROFILE_URL: process.env.UPDATE_PROFILE_URL,
+    UPDATE_URL: process.env.UPDATE_URL,
+    RECOVERY_URL: process.env.RECOVERY_URL,
+    RESET_URL: process.env.RESET_URL,
+    RESEND_URL: process.env.RESEND_URL,
+    CONFIRM_URL: process.env.CONFIRM_URL,
+    VERIFY_URL: process.env.VERIFY_URL,
+    REFRESH_URL: process.env.REFRESH_URL,
+    LOGIN_URL: process.env.LOGIN_URL,
+    LOGINAUTH_URL: process.env.LOGINAUTH_URL,
+    SIGNUP_URL: process.env.SIGNUP_URL,
+    LOGOUT_URL: process.env.LOGOUT_URL,
+    ADMIN_URL: process.env.ADMIN_URL,
+    PENDING_URL: process.env.PENDING_URL,
+    REJECTED_URL: process.env.REJECTED_URL,
+    TASKS_URL: process.env.TASKS_URL,
+    ADMIN: process.env.ADMIN,
+    SUBSCRIPTIONS_URL: process.env.SUBSCRIPTIONS_URL,
+    UNSUBSCRIBE_URL: process.env.UNSUBSCRIBE_URL,
   },
   images: {
     domains: ['res.cloudinary.com', 'cdn.pixabay.com'],
   },
   experimental: {
-    images: { allowFutureImage: true },
     largePageDataBytes: 128 * 100000,
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
